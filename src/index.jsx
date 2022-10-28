e7e94360f1b6674d20aada45981e7ff6d6fb5afd
Hiten Kumar <hitenkumarvendani@gmail.com>
2022-10-28 07:09:26 -0700

    updated

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hi(){
+
+  return <div> 
+    
+    Hello Hiten Vendani !
+    <h1>Some more text</h1>
+    
+     
+    
+    
+    </div>
+
+}
+
+ReactDOM.render(<Hi/>,document.querySelector('#root'));
